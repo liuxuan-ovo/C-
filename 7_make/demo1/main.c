25fa00530e688ad1f6a59bb7b30276f137c63adf
有点小迷糊 <103038747+liuxuan-ovo@users.noreply.github.com>
2022-04-15 20:08:16 +0800

    Add files via upload

---
+++ 7_make/demo1/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"main.h"
+#include"printf1.h"
+int main(void)
+{
+	printf("hello make world\n");
+	printf("PI=%lf\n",PI);
+	printf1();
+	return 0;
+}
---
+++ 7_make/demo1/printf1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include"main.h"
+void printf1(void)
+{
+	printf("hello priintf1 world PI=%lf\n",PI);
+}
